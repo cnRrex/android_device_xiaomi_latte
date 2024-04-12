2ed525cfb06b78a7bd1c536b032f03745a6c3500
Rrex <1837859086@qq.com>
2024-04-12 18:45:03 +0800

    update patches script

--- patches/check.sh
+++ patches/check.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rootdirectory="$PWD"
-dirs="system/core frameworks/base external/libdrm external/ffmpeg external/stagefright-plugins" 
+dirs="system/core frameworks/base external/libdrm external/ffmpeg external/stagefright-plugins bootable/recovery-twrp" 
 
 
 RED='\033[0;31m'
--- patches/uninstall.sh
+++ patches/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rootdirectory="$PWD"
-dirs="system/core frameworks/base external/libdrm external/ffmpeg external/stagefright-plugins" 
+dirs="system/core frameworks/base external/libdrm external/ffmpeg external/stagefright-plugins bootable/recovery-twrp" 
 
 
 for dir in $dirs ; do
